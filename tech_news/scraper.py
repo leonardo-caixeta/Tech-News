bd612ee65aca26a902de329fa063730b4fa9d700
leonardo <leonardocaixeta07@gmail.com>
2024-03-13 15:54:57 -0300

    feat: requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,20 @@
 # Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    time.sleep(1)
+    headers = {"user-agent": "Fake user-agent"}
+    time.sleep(1)
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
