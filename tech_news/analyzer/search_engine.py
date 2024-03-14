82733fba228f657c245f1b2ae405b4c45da9f0d7
leonardo <leonardocaixeta07@gmail.com>
2024-03-14 17:21:47 -0300

    feat: requsitos 7-9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,48 @@
+from datetime import datetime
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        result = db.news.find(
+            {"title": {"$regex": title, "$options": "i"}},
+            {"title": 1, "url": 1}
+        )
+
+        news_list = [(el["title"], el["url"]) for el in result]
+        return news_list
+    except Exception as error:
+        raise error
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")  # NOQA
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = db.news.find(
+        {"timestamp": formatted_date},
+        {"title": 1, "url": 1}
+    )
+
+    news_list = [(news["title"], news["url"]) for news in result]
+
+    return news_list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        result = db.news.find(
+            {"category": {"$regex": category, "$options": "i"}},
+            projection=["title", "url"]
+        )
+
+        news_list = [(el["title"], el["url"]) for el in result]
+        return news_list
+
+    except Exception as error:
+        raise error
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,39 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from unittest.mock import patch
 
 
-def test_reading_plan_group_news():
-    pass
+@patch("tech_news.database.find_news")
+def test_reading_plan_group_news(mock_find_news):
+    mock_find_news.return_value = [
+        {"title": "Notícia 1", "reading_time": 4},
+        {"title": "Notícia 2", "reading_time": 3},
+        {"title": "Notícia 3", "reading_time": 10},
+        {"title": "Notícia 4", "reading_time": 15},
+        {"title": "Notícia 5", "reading_time": 12},
+    ]
+
+    expected_result = {
+        "readable": [
+            {
+                "unfilled_time": 3,
+                "chosen_news": [
+                    ("Notícia 1", 4),
+                    ("Notícia 2", 3),
+                ],
+            },
+            {
+                "unfilled_time": 0,
+                "chosen_news": [
+                    ("Notícia 3", 10),
+                ],
+            },
+        ],
+        "unreadable": [
+            ("Notícia 4", 15),
+            ("Notícia 5", 12),
+        ],
+    }
+
+    service = ReadingPlanService()
+    result = service.group_news_for_available_time(10)
+    assert result == expected_result
